21394b09f05fb6b42efd8671680de018b364a284
fahmi-irfan <fahmiirfan909@gmail.com>
2017-10-30 12:41:27 +0700

    copy signup.html to signup.php

---
+++ application/views/signup.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup</title>
+    <meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css\bootstrap\bootstrap-grid.min.css">
+	<link rel="stylesheet" href="css\font-awesome-4\font-awesome.min.css">
+	<link rel="stylesheet" href="css\style_signup.css">
+</head>
+<body>
+	<div class="container-fluid" style="padding: 0px">
+		<div class="row">
+			<div class="col-md-12">
+				<nav>	<!-- navbar -->
+					<ul>
+						<li> <a href="index.html"><img src="image/logo-myBoard-w.png" style="width: 3%"><b> myBoard</b> </a></li>
+					</ul>
+				</nav>	<!-- end navbar -->
+			</div>
+		</div>
+		<div class="signup">
+			<div class="row"> <!-- Signup form -->
+				<h1 style="text-align: center;">Sign Up</h1>
+					<div class="col-md-6">
+						<p>Name</p>
+						<p>Username</p>
+						<p>Password</p>
+					</div>
+					<div class="col-md-6" style="position: relative; top: 10px; left: -50px">
+					<form style="margin-bottom: 10px">
+						<span class="signup-input"><input type="Text" name="name"></span>
+					</form>
+					<form style="margin-bottom: 10px">
+						<input type="Text" name="username">
+					</form>
+					<form style="margin-bottom: 10px">
+						<input type="Password" name="password">
+					</form>
+					<form style="margin-bottom: 10px">
+						<input type="submit" value="Sign Up" method="post" class="signup-button">
+					</form>
+					</div>
+			</div>
+		</div>	<!-- end signup form -->
+		<div class="alamat">
+			<div class="row">
+				<div class="col-md-4">
+					<p>Contact Us</p>
+					<p>0822-xxxx-xxxx</p>
+				</div>
+				<div class="col-md-4">
+					<center>
+						<p><i class="fa fa-facebook-square" aria-hidden="true"></i> @myBoard</p>
+						<p><i class="fa fa-instagram" aria-hidden="true"></i> @myBoard</p>
+						<p><i class="fa fa-twitter-square" aria-hidden="true"></i> @myBoard</p>
+					</center>
+				</div>
+				<div class="col-md-4">
+					<center>
+					<p style="text-align: right;">
+						Address<br/>
+						Bandung Sumedang Avenue KM.21,<br/>
+						Hegarmanah, Jatinangor, Sumedang District,<br/>
+						Jawa Barat 45363
+					</p>
+					</center>
+				</div>			
+			</div>
+		</div>
+	</div>
+</body>
+</html>
