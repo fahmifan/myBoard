dcb7e3c42196e75204eac3e9b44c42a53d1e8c82
fahmi-irfan <fahmiirfan909@gmail.com>
2017-10-30 12:45:18 +0700

    delete signup.html

---
+++ application/views/templates/header_home_signup.php
@@ -0,0 +1,10 @@
+<div class="container-fluid" style="padding: 0px">
+	<div class="row">
+		<div class="col-md-12">
+			<nav>	<!-- navbar -->
+				<ul>
+					<li> <a href="index.html"><img src="image/logo-myBoard-w.png" style="width: 3%"><b> myBoard</b> </a></li>
+				</ul>
+			</nav>	<!-- end navbar -->
+		</div>
+	</div>
