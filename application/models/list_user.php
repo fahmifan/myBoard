829600ff4a15f5cda79aa7864b75ab2e769b1e6a
unknown <sachihongo.sh@gmail.com>
2017-11-21 18:56:08 +0700

    Modified in list_user

--- application/models/list_user.php
+++ application/models/list_user.php
@@ -1,19 +1,26 @@
 <?php
 class list_user extends CI_Model {
 
+    //Construct
+    public function __construct() 
+    {
+        $this->load->database();
+    }
+
+    //Mendapatkan list
 	function getlist()
     {
-        $query=$this->db->query("SELECT * FROM card");
+        $query=$this->db->query("SELECT * FROM list");
         return $query->result();
     }
 
+    //Menyimpan list
     function simpan_list()
     {
         $simpan_data=array(
-            'card_desc'=> $this->input->post('nama'),
+            'list_desc'=> $this->input->post('nama'),
         );
         $simpan = $this->db->insert('list', $simpan_data);
         return $simpan;
     }
-
 }
