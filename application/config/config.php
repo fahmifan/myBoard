71a87b6cf9d832b640ba0e38da0ef3c024aa2f40
muhamistau <islam.taufikurahman@gmail.com>
2017-11-22 13:22:10 +0700

    remove index.php from URL

--- application/config/config.php
+++ application/config/config.php
@@ -34,7 +34,7 @@ $config['base_url'] = 'http://localhost/myBoard/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
